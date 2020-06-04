58866d572167702d767bd1b327227d1128da5433
mbaker-97 <mbakervtech@gmail.com>
2020-06-04 13:36:04 -0400

    Modified star and supervisord scripts

--- container/startbro.sh
+++ container/startbro.sh
@@ -8,14 +8,14 @@
 #CONTAINERINT is the interface within the Container
 CONTAINERINT=$INTERFACE
 
-sed -i 's/${INTERFACE}/'$INTERFACE' /g' /usr/local/bro/etc/node.cfg
+sed -i 's/${INTERFACE}/'$INTERFACE' /g' /usr/local/zeek/etc/node.cfg
 
-sudo chgrp $USER $(bro-config --site_dir) $(bro-config --plugin_dir)
-sudo chmod g+rwX $(bro-config --site_dir) $(bro-config --plugin_dir)
+sudo chgrp $USER $(zeek-config --site_dir) $(zeek-config --plugin_dir)
+sudo chmod g+rwX $(zeek-config --site_dir) $(zeek-config --plugin_dir)
 
-sudo chgrp $USER $(bro-config --site_dir) $(bro-config --plugin_dir)
-sudo chmod g+rwX $(bro-config --site_dir) $(bro-config --plugin_dir)
+sudo chgrp $USER $(zeek-config --site_dir) $(zeek-config --plugin_dir)
+sudo chmod g+rwX $(zeek-config --site_dir) $(zeek-config --plugin_dir)
 
-sed -i "/const fanout_id/c\ \tconst fanout_id = $RANDOM &redef;" /usr/local/bro/lib/bro/plugins/Bro_AF_Packet/scripts/init.bro
+sed -i "/const fanout_id/c\ \tconst fanout_id = $RANDOM &redef;" /usr/local/zeek/lib/zeek/plugins/Zeek_AF_Packet/scripts/init.zeek
 
-/usr/local/bro/bin/bro -i $CONTAINERINT -e 'redef LogAscii::use_json=T;'
+/usr/local/zeek/bin/zeek -i $CONTAINERINT -e 'redef LogAscii::use_json=T;'
